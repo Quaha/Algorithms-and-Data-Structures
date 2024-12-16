1abd9d8b1cb27ca895b8b5eb95d764fa3928d899
Artem Romanov <106965054+Quaha@users.noreply.github.com>
2024-12-16 23:10:47 +0300

    Update bridges and articulation points.cpp

--- Graphs/bridges and articulation points.cpp
+++ Graphs/bridges and articulation points.cpp
@@ -4,32 +4,34 @@
 
 using namespace std;
 
-namespace Bridges {
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vii = vector<pair<int, int>>;
 
-	/* 
+class Bridges {
+
+	/*
+	* Tarjan's algorithm for finding bridges in an undirected graph
+	*
+	* Time Complexity: O(N+M)
+	* Memory Complexity: O(N+M)
+	* (|V| = N, |E| = M)
+ 	*
 	* The implementation has been tested on:
 	* - https://codeforces.com/gym/100083, task C
 	*/
 
-	/* 
-	* Time Complexity: O(N+M)
-	* Memory Complexity: O(N+M)
-	*/
-
-	vector<pair<int, int>> bridges;
-	vector<int> deeps;
-
-	//Tarjan's algorithm for finding bridges in an undirected graph
-	void dfs(int curr_V, int last_V, const vector<vector<int>>& g, int curr_deep = 0) {
+private:
+	static void dfs(int curr_V, int last_V, vi& deeps, vii& bridges, const vvi& g, int curr_deep = 0) {
 
 		// Building a dfs tree
 		deeps[curr_V] = curr_deep;
 		for (int next_V : g[curr_V]) {
 			if (deeps[next_V] != -1) continue;
-			dfs(next_V, curr_V, g, curr_deep + 1);
+			dfs(next_V, curr_V, deeps, bridges, g, curr_deep + 1);
 		}
 
-		// Updating the lowest achievable depth for a current vertex
+		// Updating the lowest achievable depth from the current vertex
 		for (int next_V : g[curr_V]) {
 			if (next_V == last_V) continue;
 			deeps[curr_V] = min(deeps[curr_V], deeps[next_V]);
@@ -41,31 +43,28 @@ namespace Bridges {
 		}
 	}
 
+public:
+	Bridges() = delete;
 	/*
 	* Finds all bridges in an undirected graph.
-	* 
+	*
 	* @param g: An undirected unweighted graph given by adjacency lists
 	* @return bridges: Vector of bridges defined by incident vertices
-	* 
 	*/
-	vector<pair<int, int>> getBridges(const vector<vector<int>>& g) {
-
-		bridges.clear();
+	static vii getBridges(const vvi& g) {
 
 		int N = g.size();
 
-		deeps.resize(N, -1);
+		vii bridges(0);
+		vi deeps(N, -1);
 
 		// Bypassing all connectivity components
 		for (int start_V = 0; start_V < N; start_V++) {
 			if (deeps[start_V] == -1) {
-				dfs(start_V, start_V, g);
+				dfs(start_V, start_V, deeps, bridges, g);
 			}
 		}
 
-		deeps.clear();
-		deeps.shrink_to_fit();
-
-		return move(bridges);
+		return bridges;
 	}
-}
+};
